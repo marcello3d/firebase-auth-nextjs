84b87b7aacc95ac20274e6f7e7add01be03022e6
Marcello Bastea-Forte <marcello@cellosoft.com>
2020-08-05 23:05:12 -0700

    fix password reset flow

---
+++ src/components/NewPasswordForm.tsx
@@ -0,0 +1,54 @@
+import { FormEvent, useCallback, useRef, useState } from 'react';
+import styles from './Form.module.css';
+import { auth } from '../lib/auth';
+import { useDoPromise } from '../hooks/useDoPromise';
+import { ActionButton } from './ActionButton';
+
+export function NewPasswordForm({
+  oobCode,
+  onReset,
+}: {
+  oobCode: string;
+  onReset: () => void;
+}) {
+  const [doAction, processing, error, setError] = useDoPromise();
+  const password = useRef<HTMLInputElement>();
+  const password2 = useRef<HTMLInputElement>();
+
+  const signupWithEmail = useCallback(
+    (event: FormEvent) => {
+      if (password2.current.value !== password.current.value) {
+        setError(new Error('PASSWORDS_DO_NOT_MATCH'));
+      } else {
+        doAction(() =>
+          auth.resetPassword(oobCode, password.current.value).then(onReset),
+        );
+      }
+      event.preventDefault();
+    },
+    [oobCode],
+  );
+
+  return (
+    <form onSubmit={signupWithEmail} className={styles.loginForm}>
+      <div>
+        <label>
+          New password:{' '}
+          <input type="password" ref={password} autoComplete="new-password" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Re-type password:{' '}
+          <input type="password" ref={password2} autoComplete="new-password" />
+        </label>
+      </div>
+      <ActionButton
+        label="Change password"
+        actionLabel="Changing…"
+        processing={processing}
+        error={error}
+      />
+    </form>
+  );
+}
--- src/pages/auth/callback.tsx
+++ src/pages/auth/callback.tsx
@@ -1,22 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { auth } from '../../lib/auth';
 import styles from '../../styles/Home.module.css';
 import Head from 'next/head';
+import { NextPageContext } from 'next';
+import { ResetPasswordForm } from '../../components/ResetPasswordForm';
+import { NewPasswordForm } from '../../components/NewPasswordForm';
 
 export default function AuthCallback() {
   const router = useRouter();
   const [error, setError] = useState<any>(undefined);
+  const { mode, oobCode } = router.query;
+  const resetPasswordMode = mode === 'resetPassword';
+  const onReset = useCallback(() => {
+    router.replace('/');
+  }, [router]);
   useEffect(() => {
-    auth
-      .handleSignInRedirect()
-      .then(() => {
-        router.replace('/');
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  }, []);
+    if (!resetPasswordMode) {
+      auth
+        .handleSignInRedirect()
+        .then(onReset)
+        .catch((error) => {
+          setError(error);
+        });
+    }
+  }, [resetPasswordMode, onReset]);
   return (
     <div className={styles.container}>
       <Head>
@@ -27,13 +35,17 @@ export default function AuthCallback() {
       <main className={styles.main}>
         <h1 className={styles.title}>Firebase Auth + Next.js</h1>
 
-        <p className={styles.description}>
-          {error ? (
-            <>Authorization error: {error.message}</>
-          ) : (
-            <>Authorizing, please wait…</>
-          )}
-        </p>
+        {resetPasswordMode && typeof oobCode === 'string' ? (
+          <NewPasswordForm oobCode={oobCode} onReset={onReset} />
+        ) : (
+          <p className={styles.description}>
+            {error ? (
+              <>Authorization error: {error.message}</>
+            ) : (
+              <>Authorizing, please wait…</>
+            )}
+          </p>
+        )}
       </main>
 
       <footer className={styles.footer}>Cool cool cool.</footer>
